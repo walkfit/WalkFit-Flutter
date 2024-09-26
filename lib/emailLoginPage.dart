9aa466f85ca9b680f4538c7e12d102ba1d9e2194
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-09-26 18:10:36 +0900

    4

--- lib/emailLoginPage.dart
+++ lib/emailLoginPage.dart
@@ -2,16 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:walkfit/logo.dart';
 import 'package:walkfit/startPage.dart';
 
-class EmailLoginPage extends StatefulWidget {
+class EmailLoginPage extends StatelessWidget {
   const EmailLoginPage({super.key});
 
-  @override
-  State<EmailLoginPage> createState() => _EmailLoginPageState();
-}
-
-class _EmailLoginPageState extends State<EmailLoginPage> {
-  late final String email;
-  late final String password;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,89 +43,157 @@ class _EmailLoginPageState extends State<EmailLoginPage> {
           ),
         ],
       ),
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: SingleChildScrollView(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Column(
-                children: [
-                  const SizedBox(
-                    height: 31,
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 31,
+            ),
+            const Logo(fontSize: 50),
+            const SizedBox(
+              height: 67,
+            ),
+            SizedBox(
+              width: 350,
+              height: 50,
+              child: TextField(
+                onChanged: (value) {},
+                decoration: InputDecoration(
+                  prefixIcon: Image.asset(
+                    'assets/images/ic_outline-email.png',
                   ),
-                  const Logo(fontSize: 50),
-                  const SizedBox(
-                    height: 67,
-                  ),
-                  SizedBox(
-                    width: 350,
-                    height: 50,
-                    child: TextField(
-                      onChanged: (value) {},
-                      decoration: InputDecoration(
-                        prefixIcon: Image.asset(
-                          'assets/images/ic_outline-email.png',
-                        ),
-                        hintText: '이메일',
-                        border: const OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: 350,
-                    height: 50,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        prefixIcon: Image.asset('assets/images/uil_lock.png'),
-                        hintText: '비밀번호',
-                        border: const OutlineInputBorder(),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    width: 350,
-                    height: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    child: const Center(
-                      child: Text(
-                        '로그인',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 14,
-                  ),
-                  const Row(
-                    children: [
-                      Expanded(
-                        child: Text(
-                          'datasdsdsdsd',
-                          textAlign: TextAlign.end,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+                  hintText: '이메일',
+                  border: const OutlineInputBorder(),
+                ),
               ),
-            ],
-          ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: 350,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Image.asset('assets/images/uil_lock.png'),
+                  hintText: '비밀번호',
+                  border: const OutlineInputBorder(),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: 350,
+              height: 50,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Theme.of(context).primaryColor,
+              ),
+              child: const Center(
+                child: Text(
+                  '로그인',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 14,
+            ),
+            const Text(
+              'data',
+            )
+          ],
         ),
       ),
+      // body: GestureDetector(
+      //   onTap: () => FocusScope.of(context).unfocus(),
+      //   child: SingleChildScrollView(
+      //     child: Row(
+      //       mainAxisAlignment: MainAxisAlignment.center,
+      //       children: [
+      //         Column(
+      //           children: [
+      //             const SizedBox(
+      //               height: 31,
+      //             ),
+      //             const Logo(fontSize: 50),
+      //             const SizedBox(
+      //               height: 67,
+      //             ),
+      //             SizedBox(
+      //               width: 350,
+      //               height: 50,
+      //               child: TextField(
+      //                 onChanged: (value) {},
+      //                 decoration: InputDecoration(
+      //                   prefixIcon: Image.asset(
+      //                     'assets/images/ic_outline-email.png',
+      //                   ),
+      //                   hintText: '이메일',
+      //                   border: const OutlineInputBorder(),
+      //                 ),
+      //               ),
+      //             ),
+      //             const SizedBox(
+      //               height: 20,
+      //             ),
+      //             SizedBox(
+      //               width: 350,
+      //               height: 50,
+      //               child: TextField(
+      //                 decoration: InputDecoration(
+      //                   prefixIcon: Image.asset('assets/images/uil_lock.png'),
+      //                   hintText: '비밀번호',
+      //                   border: const OutlineInputBorder(),
+      //                 ),
+      //               ),
+      //             ),
+      //             const SizedBox(
+      //               height: 20,
+      //             ),
+      //             Container(
+      //               width: 350,
+      //               height: 50,
+      //               decoration: BoxDecoration(
+      //                 borderRadius: BorderRadius.circular(8),
+      //                 color: Theme.of(context).primaryColor,
+      //               ),
+      //               child: const Center(
+      //                 child: Text(
+      //                   '로그인',
+      //                   textAlign: TextAlign.center,
+      //                   style: TextStyle(
+      //                     color: Colors.white,
+      //                     fontSize: 16,
+      //                   ),
+      //                 ),
+      //               ),
+      //             ),
+      //             const SizedBox(
+      //               height: 14,
+      //             ),
+      //             const Row(
+      //               children: [
+      //                 Expanded(
+      //                   child: Text(
+      //                     'datasdsdsdsd',
+      //                     textAlign: TextAlign.end,
+      //                   ),
+      //                 ),
+      //               ],
+      //             ),
+      //           ],
+      //         ),
+      //       ],
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
