9b39fb438a6a5c0be9f076afae8c750ccb59fce7
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-10-10 22:51:45 +0900

    :construction: Test appBar Widget

--- lib/widgetTest.dart
+++ lib/widgetTest.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:walkfit/aPage.dart';
 import 'package:walkfit/appBarStyle.dart';
 
 void main() {
-  runApp(const WidgetTest());
+  runApp(const MyWidget());
+}
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Apage(),
+    );
+  }
 }
 
 class WidgetTest extends StatelessWidget {
@@ -13,8 +23,35 @@ class WidgetTest extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(30),
-        child: Apage(),
+        preferredSize: Size.fromHeight(30), // 앱바 높이 조절
+        child: AppBarStyle(
+          appBarTitle: "asdf",
+          appBarActions: true,
+        ),
+      ),
+    );
+  }
+}
+
+class Apage extends StatelessWidget {
+  const Apage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('data'),
+      ),
+      body: IconButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const WidgetTest(),
+            ),
+          );
+        },
+        icon: const Icon(Icons.abc),
       ),
     );
   }
