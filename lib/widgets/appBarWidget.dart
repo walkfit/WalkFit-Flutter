1523cc0394b29d9e63915db2a8a1c4ff6d4c9edd
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-10-22 16:14:01 +0900

    design :: 이메일 페이지 퍼블리싱 완료

--- lib/widgets/appBarWidget.dart
+++ lib/widgets/appBarWidget.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class AppBarwidget extends StatelessWidget {
+class AppBarWidget extends StatelessWidget {
   final String title;
   final bool actions;
-  const AppBarwidget({super.key, required this.title, this.actions = false});
+  const AppBarWidget({super.key, required this.title, this.actions = false});
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +12,26 @@ class AppBarwidget extends StatelessWidget {
       backgroundColor: Colors.white,
       leading: IconButton(
         onPressed: () => Navigator.pop(context),
-        icon: SvgPicture.asset('assets/images/arrow-outlined.svg'),
+        icon: SvgPicture.asset('assets/images/weui_arrow-outlined.svg'),
       ),
-      title: Text(title),
-      actions: actions ? [SvgPicture.asset('assets/images/bx_bell.svg')] : null,
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 20,
+          fontFamily: 'Pretendard Variable',
+          fontWeight: FontWeight.w600,
+          height: 0.07,
+          letterSpacing: -0.20,
+        ),
+      ),
+      actions: actions
+          ? [
+              IconButton(
+                onPressed: () {},
+                icon: SvgPicture.asset('assets/images/bx_bell.svg'),
+              ),
+            ]
+          : const [SizedBox()],
       centerTitle: true,
     );
   }
