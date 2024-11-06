8c6f4a0a97b2fa6862cd90130de544a425f7a7df
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-11-06 20:13:37 +0900

    design :: 출석체크 페이지 퍼블리싱 완료

--- lib/home/attendanceCheckPage.dart
+++ lib/home/attendanceCheckPage.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:walkfit/widgets/appBarWidget.dart';
+import 'package:walkfit/widgets/bottomBarWidget.dart';
 
 class AttendanceCheckPage extends StatelessWidget {
   const AttendanceCheckPage({super.key});
@@ -17,67 +18,73 @@ class AttendanceCheckPage extends StatelessWidget {
       ),
       body: Container(
         padding: EdgeInsets.symmetric(horizontal: 20.w),
-        child: Column(
+        child: Stack(
           children: [
-            SizedBox(height: 24.h),
-            TableCalendar(
-              locale: 'ko-KR',
-              focusedDay: DateTime.now(),
-              firstDay: DateTime(2023),
-              lastDay: DateTime(2025),
-              headerStyle: HeaderStyle(
-                formatButtonVisible: false, // 월/주 버튼 여부
-                leftChevronVisible: false,
-                titleCentered: true,
-                headerPadding: EdgeInsets.symmetric(vertical: 28.w),
-                titleTextFormatter: (date, locale) => // 타이틀 날짜 형태
-                    DateFormat.M(locale).format(date),
-                titleTextStyle: TextStyle(
-                  color: const Color(0xFF212121),
-                  fontSize: 18.sp,
-                  fontVariations: const [FontVariation('wght', 600)],
-                ),
-                rightChevronPadding: EdgeInsets.zero,
-                rightChevronIcon:
-                    SvgPicture.asset('assets/images/bxs_up-arrow.svg'),
-              ),
-              calendarStyle: CalendarStyle(
-                defaultTextStyle: TextStyle(
-                  color: const Color(0xFF212F83),
-                  fontSize: 16.sp,
-                  fontVariations: const [FontVariation('wght', 900)],
-                ),
-                weekendTextStyle: TextStyle(
-                  color: const Color(0xFF212F83),
-                  fontSize: 16.sp,
-                  fontVariations: const [FontVariation('wght', 900)],
-                ),
-                todayTextStyle: const TextStyle(
-                  color: Color(0xFF212F83),
-                  fontVariations: [FontVariation('wght', 900)],
-                ),
-                todayDecoration: const ShapeDecoration(
-                  shape: CircleBorder(
-                    side: BorderSide(color: Color(0xFF212F83)),
+            SizedBox(height: 54.6.h, width: 400),
+            Column(
+              children: [
+                SizedBox(height: 44.h),
+                TableCalendar(
+                  daysOfWeekHeight: 42,
+                  locale: 'ko-KR',
+                  focusedDay: DateTime.now(),
+                  firstDay: DateTime(2023),
+                  lastDay: DateTime(2025),
+                  headerStyle: HeaderStyle(
+                    formatButtonVisible: false, // 월/주 버튼 여부
+                    leftChevronVisible: false,
+                    titleCentered: true,
+                    titleTextFormatter: (date, locale) => // 타이틀 날짜 형태
+                        DateFormat.M(locale).format(date),
+                    titleTextStyle: TextStyle(
+                      color: const Color(0xFF212121),
+                      fontSize: 18.sp,
+                      fontVariations: const [FontVariation('wght', 600)],
+                    ),
+                    rightChevronPadding: EdgeInsets.zero,
+                    rightChevronIcon:
+                        SvgPicture.asset('assets/images/bxs_up-arrow.svg'),
+                  ),
+                  calendarStyle: CalendarStyle(
+                    defaultTextStyle: TextStyle(
+                      color: const Color(0xFF212F83),
+                      fontSize: 16.sp,
+                      fontVariations: const [FontVariation('wght', 900)],
+                    ),
+                    weekendTextStyle: TextStyle(
+                      color: const Color(0xFF212F83),
+                      fontSize: 16.sp,
+                      fontVariations: const [FontVariation('wght', 900)],
+                    ),
+                    todayTextStyle: const TextStyle(
+                      color: Color(0xFF212F83),
+                      fontVariations: [FontVariation('wght', 900)],
+                    ),
+                    todayDecoration: const ShapeDecoration(
+                      shape: CircleBorder(
+                        side: BorderSide(color: Color(0xFF212F83)),
+                      ),
+                    ),
+                  ),
+                  daysOfWeekStyle: DaysOfWeekStyle(
+                    weekdayStyle: TextStyle(
+                      color: const Color(0xFF212121),
+                      fontSize: 16.sp,
+                      fontVariations: const [FontVariation('wght', 500)],
+                    ),
+                    weekendStyle: TextStyle(
+                      color: const Color(0xFF212121),
+                      fontSize: 16.sp,
+                      fontVariations: const [FontVariation('wght', 500)],
+                    ),
                   ),
                 ),
-              ),
-              daysOfWeekStyle: DaysOfWeekStyle(
-                weekdayStyle: TextStyle(
-                  color: const Color(0xFF212121),
-                  fontSize: 16.sp,
-                  fontVariations: const [FontVariation('wght', 500)],
-                ),
-                weekendStyle: TextStyle(
-                  color: const Color(0xFF212121),
-                  fontSize: 16.sp,
-                  fontVariations: const [FontVariation('wght', 500)],
-                ),
-              ),
+              ],
             ),
           ],
         ),
       ),
+      bottomNavigationBar: const BottomBarWidget(),
     );
   }
 }
