ad990abd4ab1e012d2543937fd7d0294b2eded1e
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-12-19 19:58:17 +0900

    feat :: 구글 로그인 구현
    firebase를 이용해 구글 로그인 구현

--- lib/auth/googleLogin.dart
+++ lib/auth/googleLogin.dart
@@ -5,31 +5,24 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
-class googleLogin{
-  Future<User?> signInGoogle() async {
-    try {
-      // 구글 로그인
-      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-      if (googleUser == null) {
-        return null;
-      }
-
-      final GoogleSignInAuthentication googleAuth =
-      await googleUser.authentication;
-
-      log('idToken = ${googleAuth.idToken}\naccessToken = ${googleAuth
-          .accessToken}');
-      // Firebase로 인증
-      final OAuthCredential credential = GoogleAuthProvider.credential(
-        accessToken: googleAuth.accessToken,
-        idToken: googleAuth.idToken,
-      );
-      UserCredential userCredential =
-      await FirebaseAuth.instance.signInWithCredential(credential);
-      return userCredential.user;
-    } catch (e) {
-      log('$e');
-      return null;
-    }
+Future<User?> signInGoogle() async {
+  // 구글 로그인
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  if (googleUser == null) {
+    return null;
   }
+  log('success!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+  final GoogleSignInAuthentication googleAuth =
+  await googleUser.authentication;
+  // Firebase로 인증
+  final OAuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth.accessToken,
+    idToken: googleAuth.idToken,
+  );
+  UserCredential userCredential = await FirebaseAuth.instance.signInWithCredential(credential);
+  return userCredential.user;
+}
+
+void signOutGoogle() async{
+  await GoogleSignIn().signOut();
 }
