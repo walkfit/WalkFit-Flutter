b98d4657b5f6041bfdf130a7e1cb9a8fd716b844
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-12-19 17:26:27 +0900

    chore :: 파일 이동
    auth폴더로 firebase_options.dart, googleLogin.dart파일 이동

---
+++ lib/auth/googleLogin.dart
@@ -0,0 +1,35 @@
+import 'dart:developer';
+
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
+
+class googleLogin{
+  Future<User?> signInGoogle() async {
+    try {
+      // 구글 로그인
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      if (googleUser == null) {
+        return null;
+      }
+
+      final GoogleSignInAuthentication googleAuth =
+      await googleUser.authentication;
+
+      log('idToken = ${googleAuth.idToken}\naccessToken = ${googleAuth
+          .accessToken}');
+      // Firebase로 인증
+      final OAuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      UserCredential userCredential =
+      await FirebaseAuth.instance.signInWithCredential(credential);
+      return userCredential.user;
+    } catch (e) {
+      log('$e');
+      return null;
+    }
+  }
+}
